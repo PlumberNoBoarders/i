194e1e932567c62c00ef0891e664810cf0c6cf25
muanziri <munaziribienaime@yahoo.com>
2024-04-29 08:09:04 +0000

    zkxnzkjcn

--- src/views/Index.js
+++ src/views/Index.js
@@ -61,14 +61,15 @@ function Index() {
   React.useEffect(() => {
     const FetchUser=(async ()=>{
       const response = await fetch(`https://${url}/user`, {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'include', // include, *same-origin, omit
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-        }
+        },
+        body:JSON.stringify({'hello':'hewwe'})
       });
       const userResponce = await response.json();
       if(userResponce){
